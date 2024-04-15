319be503bece7d84cbe10a6a48353ae2e49def3e
Sagar Gowda <46260031+sagargowda88@users.noreply.github.com>
2024-04-15 17:03:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,41 @@ connection = pyodbc.connect('DRIVER={YourDriver};SERVER=YourServer;DATABASE=Your
 # Global variables for caching results
 dataset_cache = {}
 
+# Function to retrieve dataset names from the first query
+def get_dataset_names():
+    sql_query = "SELECT DISTINCT dataset_name FROM YourTable1 WHERE condition = 'YourCondition'"
+    cursor = connection.cursor()
+    cursor.execute(sql_query)
+    dataset_names = [row.dataset_name for row in cursor.fetchall()]
+    cursor.close()
+    return dataset_names
+
+# Function to retrieve dataset definition IDs from the second query
+def get_dataset_definition_ids(dataset_name):
+    sql_query = "SELECT dataset_definition_id FROM YourTable2 WHERE dataset_name = ? AND condition = 'YourCondition'"
+    cursor = connection.cursor()
+    cursor.execute(sql_query, (dataset_name,))
+    dataset_definition_ids = [row.dataset_definition_id for row in cursor.fetchall()]
+    cursor.close()
+    return dataset_definition_ids
+
+# Function to retrieve attributes from the third query
+def get_attributes(dataset_name):
+    sql_query = """
+        SELECT attribute_name, attribute_datatype
+        FROM YourTable3
+        WHERE dataset_definition_id IN (
+            SELECT dataset_definition_id
+            FROM YourTable2
+            WHERE dataset_name = ? AND condition = 'YourCondition'
+        )
+    """
+    cursor = connection.cursor()
+    cursor.execute(sql_query, (dataset_name,))
+    attributes = cursor.fetchall()
+    cursor.close()
+    return attributes
+
 # Function to retrieve data either from cache or pyodbc
 def get_data(dataset_name):
     global dataset_cache
@@ -14,63 +49,32 @@ def get_data(dataset_name):
     if dataset_name in dataset_cache:
         return dataset_cache[dataset_name]
     else:
-        # SQL queries
-        sql_query_1 = "SELECT DISTINCT dataset_name FROM YourTable1 WHERE dataset_name = ? AND condition = 'YourCondition'"
-        sql_query_2 = """
-            SELECT dataset_definition_id
-            FROM YourTable2
-            WHERE dataset_name = ? AND dataset_definition_id IN (
-                SELECT dataset_definition_id
-                FROM YourTable2
-                WHERE dataset_name = ? AND condition = 'YourCondition'
-            )
-        """
-        sql_query_3 = """
-            SELECT attribute_name, attribute_datatype
-            FROM YourTable3
-            WHERE dataset_definition_id IN (
-                SELECT dataset_definition_id
-                FROM YourTable2
-                WHERE dataset_name = ? AND condition = 'YourCondition'
-            )
-        """
-        
         # Execute SQL queries
-        cursor = connection.cursor()
-        
-        # Execute SQL query 1
-        cursor.execute(sql_query_1, (dataset_name,))
-        dataset_names = [row.dataset_name for row in cursor.fetchall()]
-        
-        # Execute SQL query 2
-        cursor.execute(sql_query_2, (dataset_name, dataset_name))
-        dataset_definition_ids = [row.dataset_definition_id for row in cursor.fetchall()]
-        
-        # Execute SQL query 3
-        cursor.execute(sql_query_3, (dataset_name,))
-        attributes = cursor.fetchall()
-        
-        # Close cursor
-        cursor.close()
+        dataset_names = get_dataset_names()
+        dataset_definition_ids = get_dataset_definition_ids(dataset_name)
+        attributes = get_attributes(dataset_name)
         
         # Update cache
-        dataset_cache[dataset_name] = (dataset_definition_ids, attributes)
+        dataset_cache[dataset_name] = (dataset_names, dataset_definition_ids, attributes)
     
     return dataset_cache[dataset_name]
 
 # Function to format the result
-def format_result(attributes):
-    dataset_attributes = []
-    for attribute in attributes:
-        dataset_attributes.append(f"{attribute.attribute_name} {attribute.attribute_datatype}")
+def format_result(dataset_names, dataset_definition_ids, attributes):
+    formatted_result = ""
+    for dataset_name in dataset_names:
+        formatted_result += f"{dataset_name}\n"
+        for dataset_definition_id in dataset_definition_ids:
+            formatted_result += f"{dataset_definition_id}\n"
+        for attribute in attributes:
+            formatted_result += f"{attribute.attribute_name} {attribute.attribute_datatype}\n"
     
-    formatted_attributes = ",\n".join(dataset_attributes)
-    return formatted_attributes
+    return formatted_result
 
 # Example usage
 def get_and_format_data(dataset_name):
-    dataset_definition_ids, attributes = get_data(dataset_name)
-    return format_result(attributes)
+    dataset_names, dataset_definition_ids, attributes = get_data(dataset_name)
+    return format_result(dataset_names, dataset_definition_ids, attributes)
 
 result = get_and_format_data(dataset_name="YourDatasetName")
 print(result)
